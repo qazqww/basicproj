951e8bb93b849267b85d4234838ebcbc07175440
raptarior <qazqww@naver.com>
2024-09-05 17:09:36 +0900

    first commit

---
+++ src/main/java/com/raptarior/basicproj/BasicprojApplication.java
@@ -0,0 +1,13 @@
+package com.raptarior.basicproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raptarior/basicproj/BasicprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raptarior.basicproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
